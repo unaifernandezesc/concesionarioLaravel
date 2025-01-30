1114a416e748176795a0e11d5cfc78c31685f75b
Unai <you@example.com>
2025-01-30 19:46:09 +0100

    Modificar CSS

--- resources/views/crearCoche.blade.php
+++ resources/views/crearCoche.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('CSS/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>Coches</title>
 </head>
 <body>
